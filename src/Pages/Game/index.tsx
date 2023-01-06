d2a6465af45c7f03a47a4ff4310da330165e70d8
anafmonteiro <ana.monteiro2014@engenharia.ufjf.br>
2023-01-06 15:41:49 -0300

    include questions, display and feedback style

--- src/Pages/Game/index.tsx
+++ src/Pages/Game/index.tsx
@@ -4,21 +4,22 @@ import HeadsUpComponent from "../../Components/HeadsUp";
 import OptionBoxComponent from "../../Components/OptionBox";
 import { 
     Container, 
+    QuestionContainer,
     SubTitle,
 } from "./styles";
 
 const GamePage: React.FC = () => {
-
-    const [question, setQuestions] = useState<{question:string, answers:string[]}>({question: "", answers:[""]})
+    const [question, setQuestions] = useState<{question:string, correct_answer:string, answers:string[]}>({question: "", correct_answer: "", answers:[""]})
 
     const getAllQuestions = async () => {
         const response:any = await getQuestions()
-        
-        response.map((question: any) => {
+        response.map((question: any) => (
             setQuestions({
-                question: question.question, answers: [...question.incorrect_answers, question.correct_answer]})
-        })
-        
+                question: question.question, 
+                correct_answer: question.correct_answer, 
+                answers: [...question.incorrect_answers, question.correct_answer]
+            })
+        ))
     }
 
     useEffect(()=>{
@@ -28,10 +29,18 @@ const GamePage: React.FC = () => {
     return(
         <Container>
             <HeadsUpComponent/>
+            <QuestionContainer>
                 <SubTitle>{question.question}</SubTitle>
-                <OptionBoxComponent
-                    answers={question.answers}
-                />
+                {question.answers.map((answer, index) => (
+                    <OptionBoxComponent
+                        onClick={getAllQuestions}
+                        key={index}
+                        prefix={++index}
+                        answer={answer}
+                        correctAnswer={question.correct_answer}
+                    />
+                ))}
+            </QuestionContainer>
         </Container>
     )
 }
